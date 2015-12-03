85cf42c87c47689281352e0f28359140ffffb2ce
Yutaka Tsutano <yutaka@tsutano.com>
2015-12-03 05:08:57 -0600

    Remove unnecessary creation of NSImage

--- GeneratePreviewForURL.m
+++ GeneratePreviewForURL.m
@@ -14,37 +14,31 @@
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-	
-	NSData *imageData = [Dot dataFromDotFile: (__bridge NSURL *)url];
-	NSImage *imageForSize = [[NSImage alloc] initWithData: imageData];
-	
-	CGSize canvasSize = CGSizeMake(imageForSize.size.width,imageForSize.size.height);
+    NSData *imageData = [Dot dataFromDotFile: (__bridge NSURL *)url];
 
     @autoreleasepool {
-        // Preview will be drawn in a vectorized context
-        CGContextRef cgContext = QLPreviewRequestCreateContext(preview, *(CGSize *)&canvasSize, false, NULL);
-        if(cgContext) { 
-            CGDataProviderRef imgDataProvider = CGDataProviderCreateWithCFData ((__bridge CFDataRef)imageData);
-            CGPDFDocumentRef document = CGPDFDocumentCreateWithProvider(imgDataProvider);
-			
-            if (CGPDFDocumentGetNumberOfPages(document) > 0) {
-                CGPDFPageRef page = CGPDFDocumentGetPage(document, 1);
+        CGDataProviderRef imgDataProvider = CGDataProviderCreateWithCFData((__bridge CFDataRef)imageData);
+        CGPDFDocumentRef document = CGPDFDocumentCreateWithProvider(imgDataProvider);
+
+        if (CGPDFDocumentGetNumberOfPages(document) > 0) {
+            CGPDFPageRef page = CGPDFDocumentGetPage(document, 1);
+            CGRect pageRect = CGPDFPageGetBoxRect(page, kCGPDFMediaBox);
+
+            CGContextRef cgContext = QLPreviewRequestCreateContext(preview, *(CGSize *)&pageRect.size, false, NULL);
+            if (cgContext) {
                 CGContextDrawPDFPage(cgContext, page);
+                QLPreviewRequestFlushContext(preview, cgContext);
+
+                CFRelease(cgContext);
             }
-			
-            QLPreviewRequestFlushContext(preview, cgContext);
-            
-            CGPDFDocumentRelease(document);
-            CGDataProviderRelease(imgDataProvider);
-            CFRelease(cgContext);
         }
+
+        CGPDFDocumentRelease(document);
+        CGDataProviderRelease(imgDataProvider);
     }
-	
+
 	return noErr;
 }
-	
-
-
 
 void CancelPreviewGeneration(void* thisInterface, QLPreviewRequestRef preview)
 {
