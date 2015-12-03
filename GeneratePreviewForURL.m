5533287a53f9ea2fbd8da25156abfc8d2bc8f740
Yutaka Tsutano <yutaka@tsutano.com>
2015-12-03 05:49:59 -0600

    Use QLPreviewRequestSetDataRepresentation() instead of drawing

--- GeneratePreviewForURL.m
+++ GeneratePreviewForURL.m
@@ -14,28 +14,8 @@
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-    NSData *imageData = [Dot dataFromDotFile: (__bridge NSURL *)url];
-
-    @autoreleasepool {
-        CGDataProviderRef imgDataProvider = CGDataProviderCreateWithCFData((__bridge CFDataRef)imageData);
-        CGPDFDocumentRef document = CGPDFDocumentCreateWithProvider(imgDataProvider);
-
-        if (CGPDFDocumentGetNumberOfPages(document) > 0) {
-            CGPDFPageRef page = CGPDFDocumentGetPage(document, 1);
-            CGRect pageRect = CGPDFPageGetBoxRect(page, kCGPDFMediaBox);
-
-            CGContextRef cgContext = QLPreviewRequestCreateContext(preview, *(CGSize *)&pageRect.size, false, NULL);
-            if (cgContext) {
-                CGContextDrawPDFPage(cgContext, page);
-                QLPreviewRequestFlushContext(preview, cgContext);
-
-                CFRelease(cgContext);
-            }
-        }
-
-        CGPDFDocumentRelease(document);
-        CGDataProviderRelease(imgDataProvider);
-    }
+    NSData *data = [Dot dataFromDotFile: (__bridge NSURL *)url];
+    QLPreviewRequestSetDataRepresentation(preview, (__bridge CFDataRef)data, kUTTypePDF, NULL);
 
 	return noErr;
 }
